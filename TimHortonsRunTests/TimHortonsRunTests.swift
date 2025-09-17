2d7e5a412acb9199ae82d4504a9b6b08b7880808
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-09-17 11:11:56 -0400

    Initial Commit

---
+++ TimHortonsRunTests/TimHortonsRunTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimHortonsRunTests.swift
+//  TimHortonsRunTests
+//
+//  Created by Fatom on 2025-09-17.
+//
+
+import Testing
+@testable import TimHortonsRun
+
+struct TimHortonsRunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
